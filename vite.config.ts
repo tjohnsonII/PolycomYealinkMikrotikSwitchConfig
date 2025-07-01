0c6cb20526f68b7b54ac6d7655cbeef00c5216c4
tjohnsonII <tjohnson082083@gmail.com>
2025-07-01 00:10:23 +0000

    update - update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+    open: true,
+    allowedHosts:['timsablab.com','timsablab.ddn.net']
+  },
   plugins: [react()],
 })
